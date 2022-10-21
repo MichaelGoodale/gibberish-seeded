e0d260ea789da19b3b8acdba56d7f5267475d3c8
Michael Goodale <michael.goodale@mail.mcgill.ca>
2022-10-21 15:46:59 +0200

    Added seeding

--- gibberish/__init__.py
+++ gibberish/__init__.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 import string
 import yaml
-try:
-    from secrets import choice
-except ImportError:
-    from random import choice
+import random
 
 from gibberish.dir import database_path
 
@@ -12,9 +9,10 @@ __all__ = ('Gibberish', )
 
 
 class Gibberish:
-    def __init__(self, component_file=database_path('components.yaml')):
+    def __init__(self, component_file=database_path('components.yaml'), seed=42):
         with open(component_file) as f:
             components = yaml.safe_load(f)
+        self.random = random.Random(seed)
 
         self.initial_consonants = list(set(string.ascii_lowercase) - set('aeiou')
                                   # remove those easily confused with others
@@ -42,12 +40,12 @@ class Gibberish:
             letter_list.extend([self.vowels, self.final_consonants])
         if end_vowel:
             letter_list.pop()
-        return ''.join(choice(s) for s in letter_list)
+        return ''.join(self.random.choice(s) for s in letter_list)
 
     def generate_words(self, wordcount=1, vowel_consonant_repeats=1):
         """Returns a list of ``wordcount`` pseudo-words."""
         # range for Python 3 compatibility
-        return [self.generate_word(vowel_consonant_repeats=vowel_consonant_repeats if vowel_consonant_repeats else choice(range(1,4))) for _ in range(wordcount)]
+        return [self.generate_word(vowel_consonant_repeats=vowel_consonant_repeats if vowel_consonant_repeats else self.random.choice(range(1,4))) for _ in range(wordcount)]
 
 
 def console_main():
